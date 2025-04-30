0af569fca624e1f0f426f5cf80507cefd3427c66
Ahsan Sajjad <sajjadahsan461@gmail.com>
2025-04-30 22:50:18 +0500

    Context setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
     
-    <h1>Hello World</h1>
+ 
+ <Form/>
+ <TodoList/>
+ 
     </div>
   );
 }
